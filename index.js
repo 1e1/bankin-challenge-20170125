6f275cf6121772e86e7c35e30211dafae4c146f3
aglr <aymeric.gerlier@rentacar.fr>
2018-01-26 15:17:06 +0100

    === OUT OF DATE ===
    reduce and clean code (-10%)
    refactor the multithreader
    
    speed +10%
    memory--

--- index.js
+++ index.js
@@ -106,7 +106,8 @@ const BROWSER_PROMISE = PUPPETEER.launch(PUPPETEER_ARGS);
 const NB_PARALLEL_PROCESS_PER_CORE = 8;
 const NB_PARALLEL_PROCESS = OS.cpus().length * NB_PARALLEL_PROCESS_PER_CORE;
 
-var TRANSACTION_LIST = []; // the final result
+var TRANSACTION_LIST = [];  // the final result
+var IS_CRAWLING = true;     // stop process if false
 
 
 /* --------------------------------------------------------------------------
@@ -127,75 +128,104 @@ function appendTransactionList(transactionList) {
 /**
  * iterate on each frames of the page until a frame contains a transaction list
  * @param {Page} page - the page to parse
- * @return {integer} length of the transaction list found on the page
  */
 async function parseAnyFrame(page) {
     const frames = page.frames();
+
+    let transactionList;
     
     for (let index = 0; index < frames.length; ++index) {
         const frame = frames[index];
-        const transactionList = await getTransactionList(frame);
-        const transactionListLength = transactionList.length;
+        
+        transactionList = await getTransactionList(frame);
         
         // stop now if the extractor worked
-        if (0 !== transactionListLength) {
+        if (0 !== transactionList.length) {
             appendTransactionList(transactionList);
 
-            return transactionListLength;
+            break;
         }
     };
-    
-    return 0;
+
+    if (transactionList.length !== URL_PARAMETER_START.step) {
+        IS_CRAWLING = false;
+    }
 }
 
 
 /**
- * load the URL into the page container, 
- * navigate on the page for extracting the transaction list
- * @param {Page} page - the page container
- * @param {string} url - the targeted URL
- * @param {integer} processIndex - the processIndex
- * @return {Array<integer>} 0:length of the transaction list found on the page (-1 is an error), 1: processIndex
+ * crawl while there is a new URL having transactions
+ * @throws Error("network") if a scrapper fails due to a network issue
+ * @param {Browser} browser - the browser container
  */
-async function parsePage(page, url, processIndex) {
+async function crawl(browser) {
+    const page = await newPage(browser);
+
     const reloadButtonSelector = PAGE_CONFIG.reloadButtonSelector;
     const mutableSelector = PAGE_CONFIG.mutableSelector;
 
-    let nbRetry = PAGE_CONFIG.nbPageReload;
-    let nbTransaction = -1; // -1 = error while requesting the URL
-
     const parse = async () => {
-        // stop retrying
-        nbRetry = -1;
-
         // click on the reload button if exsits
         page.click(reloadButtonSelector).catch(()=>null);
         
         // trigger on %mutableSelector%
         await page.waitForSelector(mutableSelector, WAIT_FOR_SELECTOR_OPTIONS).catch(()=>null);
-
+        
         // extract the transaction list
-        nbTransaction = await parseAnyFrame(page);
+        await parseAnyFrame(page);
     }
 
-    // retry goto(URL) x nbRetry, otherwise it fails
     do {
-        try {
-            await page.goto(url, GOTO_OPTIONS).then(parse, ()=>--nbRetry);
-        } catch(ignore) {}
-    } while (0 < nbRetry);
-    
-    return [nbTransaction, processIndex];
+        const url = getUrl();
+
+        if (null === url) {
+            IS_CRAWLING = false;
+
+            break;
+        }
+        
+        let nbRetry = PAGE_CONFIG.nbPageReload;
+
+        // retry goto(URL) x nbRetry, otherwise it fails
+        do {
+            try {
+                await page.goto(url, GOTO_OPTIONS).then(parse, ()=>--nbRetry);
+
+                nbRetry = -1;
+            } catch(ignore) {}
+        } while (0 < nbRetry);
+
+        if (0 === nbRetry) {
+            throw new Error("An network error occured during the scrapping");
+        }
+    } while(IS_CRAWLING);
 }
 
 
 /**
- * initialize a new page container from a promise
- * @param {Promise<Page>} pagePromise - the page promise
+ * return the current URL, then increment the start parameter
+ * @return {string} url
+ */
+function getUrl() {
+    if (URL_PARAMETER_START.current > URL_PARAMETER_START.max) {
+        return null;
+    }
+
+    const url = URL.format({start: URL_PARAMETER_START.current});
+
+    URL_PARAMETER_START.current += URL_PARAMETER_START.step;
+
+    return url;
+}
+
+
+/**
+ * initialize a new page container
+ * @param {Browser} browser - the page browser
  * @return {Page} page container
  */
-async function prepareNewPage(pagePromise) {
-    const page = await pagePromise;
+async function newPage(browser) {
+    const page = await browser.newPage();
     
     // dismiss blocking dialog window if exists
     page.on('dialog', async dialog => {
@@ -207,79 +237,25 @@ async function prepareNewPage(pagePromise) {
 
 
 /**
- * setup a list a page containers
- * @param {Browser} browser - the browser
- * @return {Array<Page>} array of page container
- */
-async function preparePageList(browser) {
-    const pagePromises = [];
-    const pages = [];
-
-    // start page containers
-    for (let i = 0; i < NB_PARALLEL_PROCESS; ++i) {
-        pagePromises.push(browser.newPage());
-    }
-    
-    // waiting for the page containers are ready
-    for (let i = 0; i < pagePromises.length; ++i) {
-        pages[i] = await prepareNewPage(pagePromises[i]);
-    }
-
-    return pages;
-}
-
-
-/**
  * manage the parallel work
  * @throws Error("network") if a scrapper fails due to a network issue
  * @param {Promise<Browser>} browserPromise - the browser promise
  */
 async function run(browserPromise) {
     const browser = await browserPromise;
+    const crawlerPromises = []; // pool of jobs to run in a page
 
-    const pages = await preparePageList(browser); // pool of thread-pages
-    const parserPromises = []; // pool of jobs to run in a page
-    const pageSize = URL_PARAMETER_START.step;
-    const urlParameterStartMax = URL_PARAMETER_START.max;
+    URL_PARAMETER_START.current = URL_PARAMETER_START.min;
 
-    let nbTransactionOnTheLastPage; // length of the transaction list found on the last page
-    let processIndex; // index of the thread-page in parserPromises[]
-    let urlParameterStart = URL_PARAMETER_START.min; // value of the "start" URL parameter
+    // start parallelism
+    for (processIndex = 0; processIndex < NB_PARALLEL_PROCESS; ++processIndex) {
+        const crawlerPromise = crawl(browser);
 
-    // start a new "parsePage" on the thread-page at processIndex
-    const startParserAt = (pid) => {
-        const page = pages[pid];
-        const url = URL.format({start: urlParameterStart});
-        const parserPromise = parsePage(page, url, pid);
-
-        parserPromises[pid] = parserPromise;
-        urlParameterStart += pageSize;
+        crawlerPromises.push(crawlerPromise);
     }
-    
-    // first load into the empty thread-pages
-    for (processIndex = 0; processIndex < pages.length; ++processIndex) {
-        startParserAt(processIndex);
-    }
-
-    // reload scrapper who terminated into an existing thread-pages
-    do {
-        [nbTransactionOnTheLastPage, processIndex] = await Promise.race(parserPromises);
-        
-        startParserAt(processIndex);
-    } while(pageSize === nbTransactionOnTheLastPage && urlParameterStartMax > urlParameterStart);
-    
-    // last startParserAt(page) is useless
-    delete parserPromises[processIndex];
 
     // shutdown
-    await Promise.all(parserPromises);
-
-    // the loop stops because the script can not execute page.goto several times
-    if (-1 === nbTransactionOnTheLastPage) {
-        TRANSACTION_LIST = null;
-        throw new Error("An network error occured during the scrapping");
-    }
-
+    await Promise.all(crawlerPromises);
     await browser.close();
 }
 
@@ -297,4 +273,3 @@ async function run(browserPromise) {
     // be carefull it floods the JSON response
     //console.debug(TRANSACTION_LIST.length + " transactions");
 })()
-// this is sparta
